31d9314a0940082e725f51310ee385037357b167
nicocabrerafit2 <nicocabrera8@outlook.com>
2022-11-06 23:46:42 -0300

    completo controller y rutas

--- src/routers/main.js
+++ src/routers/main.js
@@ -4,5 +4,6 @@ const mainController = require("../controllers/mainController.js");
 let router = express.Router();
 
 router.get("/", mainController.index);
+router.get("/about", mainController.about);
 
 module.exports = router;
